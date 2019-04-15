4fb6625c585a73a63a07eed755f7c225fff35ce6
wl1390 <wl1390@nyu.edu>
2019-04-15 22:33:38 +0800

    initial code for server

---
+++ server.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h> 
+#include <string.h>
+#include <stdlib.h>
+
+int load_server_argv(int argc, char **argv, int *shmid)
+{
+	int i;
+
+	if ((argc != 3) || (strcmp(argv[1], "-m"))) return 0;
+	
+	for (i = 0; i < strlen(argv[2]); i++)
+	{
+		if (!isdigit(argv[2][i])) return 0;
+	} 
+
+	*shmid = atoi(argv[2]);
+
+	return 1;
+}
+
+
+int main(int argc, char **argv)
+{
+	int shmid;
+
+	if (!load_server_argv(argc, argv, &shmid))
+	{
+		printf("ERROR: server initiation should follow the below format:\n");
+		printf("\t./server -m shmid\n");
+		
+		return 0;
+	}
+
+	printf("%d\n", shmid);
+
+	return 0;
+}
